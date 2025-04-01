b25abb6194f196ef1b743c8507e8008ebb9297d5
Sai Pranith Bhagavatula <saipranbhagavatula@gmail.com>
2025-04-01 06:16:38 +0530

    massive update: AI integration and UI updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.thevaguebox.probabilistictictactoe"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
